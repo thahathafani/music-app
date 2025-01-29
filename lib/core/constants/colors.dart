6c8fd9d1ef7aaf4936291cb6f751a9c7a30c6f97
thahathafani <thahafazzin@gmail.com>
2025-01-29 19:01:00 +0530

    colors file created and data added

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors{
+  static const primary = Color(0xFF1DB954);
+  static const background = Color(0xFF121212);
+  static const cardColor = Color(0xFF181818);
+  static const textPrimary = Color(0xFFFFFFFF);
+  static const textSecondary = Color(0xFFB3B3B3);
+}
